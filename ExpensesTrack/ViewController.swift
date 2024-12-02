51b832c6d5c4b0e1c370b9d2a2b3c411c47984b2
Alexandre Henrique da Silva <echo.alexandre@gmail.com>
2024-12-02 18:59:52 -0300

    Initial Commit

---
+++ ExpensesTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpensesTrack
+//
+//  Created by Alexandre Henrique da Silva on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
